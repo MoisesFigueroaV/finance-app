92148a5b25c1674a2e4b1dcd2fa9a465b24d842e
MoisesFigueroaV <moisesfigueroadeveloper@gmail.com>
2026-09-25 17:17:17 -0300

    add routers configurations

--- components/navigation.tsx
+++ components/navigation.tsx
@@ -1,9 +1,12 @@
 "use client";
 
-import { usePathname, useRouter } from "next/navigation";
 import { useState } from "react";
-import { NavButton } from "@/components/ui/nav-button";
 import { useMedia } from "react-use";
+import { Menu } from "lucide-react";
+import { usePathname, useRouter } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import { NavButton } from "@/components/ui/nav-button";
 import {
   Sheet,
   SheetContent,
@@ -38,6 +41,32 @@ export const Navigation = () => {
 
   const pathname = usePathname();
   const router = useRouter();
+  const isMobile = useMedia("(max-width: 1024px)", false);
+
+  const onClick = (href: string) => {
+    router.push(href);
+    setIsOpen(false);
+  }
+
+  if (isMobile) {
+    return (
+      <Sheet open={isOpen} onOpenChange={setIsOpen}>
+        <SheetTrigger>
+          <Button
+            variant="ghost"
+            size="sm"
+            className="font-normal bg-white/10 hover:bg-white/20
+            hover:text-white border-none focus-visible:ring-offset-0
+            focus-visible:ring-transparent outline-none text-white
+            focus:bg-white/30 transition border-none"
+          >
+
+          </Button>
+          <Menu />
+        </SheetTrigger>
+      </Sheet>
+    )
+  }
 
   return (
     <nav className="hidden lg:flex items-center gap-x-2 overflow-x-auto">
